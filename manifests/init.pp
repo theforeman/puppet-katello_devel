86a90ae86419b51f30722f039f189f312001be79
Dustin Tsang <dtsang@redhat.com>
2014-07-15 20:53:09 -0400

    Fixes #6544 - q to receive candlepin events
    also turns off auto load linting and corrects indentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -105,6 +105,11 @@ class katello_devel (
 
   Class['certs'] ~>
   Class['certs::qpid'] ~>
+  class { 'qpid::client': } ~>
+  class { 'katello::qpid':
+    client_cert => $certs::qpid::client_cert,
+    client_key  => $certs::qpid::client_key,
+  } ~>
   class { 'certs::pulp_parent': } ~>
   class { 'pulp':
     oauth_key                   => $katello_devel::oauth_key,
