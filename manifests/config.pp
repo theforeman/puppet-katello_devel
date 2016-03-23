7ba8f3a3994fa06f14641b41fbf66f659e83b43b
Andrew Kofink <akofink@redhat.com>
2016-03-23 14:32:35 -0400

    Add pry-byebug and puma in a Gemfile.local.rb for foreman

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,5 +1,13 @@
 # Configuration for Katello development
 class katello_devel::config {
+  file { "${katello_devel::deployment_dir}/foreman/bundler.d/Gemfile.local.rb":
+    ensure  => file,
+    content => template('katello_devel/Gemfile.local.rb.erb'),
+    owner   => $katello_devel::user,
+    group   => $katello_devel::group,
+    mode    => '0644',
+  }
+
   file { "${katello_devel::deployment_dir}/foreman/bundler.d/katello.local.rb":
     ensure  => file,
     content => template('katello_devel/katello.local.rb.erb'),
