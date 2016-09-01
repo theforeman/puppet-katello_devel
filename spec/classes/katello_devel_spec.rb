3a4b5e15aa2bbc38c310ec2d982976fe4ed5e94c
Eric D. Helms <ericdhelms@gmail.com>
2016-09-01 14:42:48 -0400

    Fix trailing whitespace errors

--- spec/classes/katello_devel_spec.rb
+++ spec/classes/katello_devel_spec.rb
@@ -4,7 +4,7 @@ describe 'katello_devel' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let :facts do
-        facts.merge(:concat_basedir => '/tmp', :mongodb_version => '2.4.14')
+        facts.merge(:concat_basedir => '/tmp', :mongodb_version => '2.4.14', :root_home => '/root')
       end
 
       let(:params) do
