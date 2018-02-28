1649f3c837a73ea024da7b5380b5a3a840adc211
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-02-28 14:58:08 +0100

    Refactor git_repo to use local variables

--- manifests/git_repo.pp
+++ manifests/git_repo.pp
@@ -1,28 +1,35 @@
 # Setup and configure a git repo
 define katello_devel::git_repo(
   String $source,
-  $github_username = undef,
-  $upstream_remote_name = $katello_devel::upstream_remote_name,
+  String $upstream_remote_name = $katello_devel::upstream_remote_name,
+  Optional[String] $github_username = undef,
+  Optional[String] $fork_remote_name = $katello_devel::fork_remote_name_real,
+  Stdlib::Absolutepath $deployment_dir = $katello_devel::deployment_dir,
+  String $dir_owner = $katello_devel::user,
+  Boolean $use_ssh_fork = $katello_devel::use_ssh_fork,
 ) {
 
-  if $github_username != undef {
-    if $katello_devel::use_ssh_fork {
+  if $github_username {
+    if $use_ssh_fork {
       $fork_url = "git@github.com:${github_username}/${title}.git"
     } else {
       $fork_url = "https://${github_username}@github.com/${github_username}/${title}.git"
     }
 
-    $sources = {"${upstream_remote_name}" => "https://github.com/${source}.git", "${katello_devel::fork_remote_name_real}" => $fork_url}
+    $sources = {
+      $upstream_remote_name => "https://github.com/${source}.git",
+      $fork_remote_name => $fork_url,
+    }
   } else {
-    $sources = {"${upstream_remote_name}" => "https://github.com/${source}.git"}
+    $sources = {$upstream_remote_name => "https://github.com/${source}.git"}
   }
 
-  vcsrepo { "${katello_devel::deployment_dir}/${title}":
+  vcsrepo { "${deployment_dir}/${title}":
     ensure   => present,
     provider => git,
     remote   => $upstream_remote_name,
     source   => $sources,
-    user     => $katello_devel::user,
+    user     => $dir_owner,
   }
 
 }
