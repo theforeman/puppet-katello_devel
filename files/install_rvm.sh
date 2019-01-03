e14c24bb418836454c1096f999f44ffbb3a36a2a
John Mitsch <jomitsch@redhat.com>
2019-01-03 16:12:41 -0500

    Update RVM gpg keys
    it looks like the gpg keys changed for rvm, we
    can update them in our script.
    
    See more info here:
    https://rvm.io/integration/vagrant#using-rvm-with-vagrant

--- files/install_rvm.sh
+++ files/install_rvm.sh
@@ -9,7 +9,7 @@ SERVERS=(
 
 for server in "${SERVERS[@]}"
 do
-    gpg --keyserver ${server} --recv-keys D39DC0E3
+    gpg --keyserver ${server} --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
     success=$?
 
     if [ "$success" == "0" ]; then
