f1f4b51cd15e7bddec9f2e8412198a7153fdac84
Andrew Kofink <akofink@redhat.com>
2019-01-30 10:22:05 -0500

    Fetch keys from the keyserver suggested by rvm

--- files/install_rvm.sh
+++ files/install_rvm.sh
@@ -5,11 +5,12 @@ RVM_BRANCH=${2:-stable}
 SERVERS=(
     hkp://pgp.mit.edu
     hkp://keys.gnupg.net
+    hkp://pool.sks-keyservers.net
 )
 
 for server in "${SERVERS[@]}"
 do
-    gpg --keyserver ${server} --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+    gpg2 --keyserver ${server} --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
     success=$?
 
     if [ "$success" == "0" ]; then
