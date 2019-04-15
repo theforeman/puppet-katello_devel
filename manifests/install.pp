413c9bb3ada1ef11a25a8b13fc8a0b0a856ee7c3
Jonathon Turel <jturel@gmail.com>
2019-04-15 09:08:38 -0400

    Install cyrus-sasl-plain

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,7 @@
 # Katello Development Install
 class katello_devel::install {
 
-  package{ ['libvirt-devel', 'sqlite-devel', 'postgresql-devel', 'libxslt-devel', 'systemd-devel', 'libxml2-devel', 'git', 'npm', 'libcurl-devel', 'gcc-c++', 'libstdc++']:
+  package{ ['cyrus-sasl-plain', 'libvirt-devel', 'sqlite-devel', 'postgresql-devel', 'libxslt-devel', 'systemd-devel', 'libxml2-devel', 'git', 'npm', 'libcurl-devel', 'gcc-c++', 'libstdc++']:
     ensure => present,
   }
 
