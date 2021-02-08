2e4d76acac11271635ba5ad983d351be729f0c3e
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-02-08 12:23:57 +0100

    Drop RVM support
    It already defaulted to OS versions since that's also used in production.

--- README.md
+++ README.md
@@ -6,11 +6,6 @@ This module is designed to setup a Katello development environment for developin
 
 * Installs Katello and Foreman from git
 * Provides an HTTPS server and proxy to local Rails server for easy use of subscription-manager
-* Uses RVM to provide isolated gem environment
-
-## Usage
-
-Please see https://github.com/Katello/katello-installer#development-examples
 
 ## Development
 
--- manifests/bundle.pp
+++ manifests/bundle.pp
@@ -1,32 +1,20 @@
-# @summary Run a bundle command, possibly under RVM or SCL
+# @summary Run a bundle command
 # @api private
 define katello_devel::bundle (
   Array[String] $environment = [],
   Variant[Undef, String[1], Array[String[1]]] $unless = undef,
-  Boolean $use_rvm = $katello_devel::use_rvm,
-  String $rvm_ruby = $katello_devel::rvm_ruby,
   String $user = $katello_devel::user,
   Stdlib::Absolutepath $cwd = $katello_devel::foreman_dir,
   Integer[0] $timeout = 600,
 ) {
-  if $use_rvm {
-    include katello_devel::rvm
-    Class['katello_devel::rvm'] -> Exec["bundle-${title}"]
-    $command = "rvm ${rvm_ruby} do bundle ${title}"
-    $path = "/home/${user}/.rvm/bin:/usr/bin:/bin"
-  } else {
-    $command = "bundle ${title}"
-    $path = '/usr/bin:/bin'
-  }
-
   exec { "bundle-${title}":
-    command     => $command,
+    command     => "bundle ${title}",
     environment => $environment + ["HOME=/home/${user}"],
     cwd         => $cwd,
     user        => $user,
     logoutput   => 'on_failure',
     timeout     => $timeout,
-    path        => $path,
+    path        => ['/usr/bin', '/bin'],
     unless      => $unless,
   }
 }
