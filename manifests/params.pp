17aa02444a8262133a98d2d974917d6fcf0e8501
Eric D. Helms <ericdhelms@gmail.com>
2021-05-06 20:46:14 -0400

    Convert parameters to static defaults

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,22 +1,13 @@
 # @summary Katello development parameters
 # @api private
 class katello_devel::params {
-  $user = undef
 
   $oauth_key          = extlib::cache_data('foreman_cache_data', 'oauth_consumer_key', extlib::random_password(32))
   $oauth_secret       = extlib::cache_data('foreman_cache_data', 'oauth_consumer_secret', extlib::random_password(32))
 
-  $deployment_dir = '/home/vagrant'
-
   $rails_port = 3000
 
-  $post_sync_token = 'test'
-
-  $webpack_dev_server = true
-
-  $use_rvm = false
-  $rvm_ruby = '2.7'
-  $rvm_branch = 'stable'
+  $extra_plugins = []
 
   if $facts['os']['release']['major'] == '7' {
     $scl_ruby = 'rh-ruby27'
@@ -27,31 +18,4 @@ class katello_devel::params {
     $scl_nodejs = undef
     $scl_postgresql = undef
   }
-
-  $manage_bundler = true
-
-  $initial_organization = 'Default Organization'
-  $initial_location = 'Default Location'
-  $admin_password = 'changeme'
-  $enable_yum = true
-  $enable_file = true
-  $enable_docker = true
-  $enable_deb = true
-  $enable_ansible_collection = true
-
-  $github_username = undef
-  $use_ssh_fork = false
-  $fork_remote_name = undef
-  $upstream_remote_name = 'upstream'
-
-  $foreman_scm_revision = 'develop'
-  $katello_scm_revision = 'master'
-
-  $extra_plugins = []
-
-  $qpid_wcache_page_size = 4
-
-  $rails_command='puma -w 2 -p $PORT --preload'
-
-  $npm_timeout = 900
 }
