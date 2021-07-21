4689d964f5c695db18c2fa1637827f88002e8e13
Eric D. Helms <ericdhelms@gmail.com>
2021-07-21 18:39:38 -0400

    Add basic acceptance test

---
+++ spec/acceptance/katello_devel_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper_acceptance'
+
+describe 'Scenario: install katello_devel' do
+  let(:manifest) do
+    <<-PUPPET
+      class { 'katello_devel':
+        user => 'vagrant',
+      }
+    PUPPET
+  end
+
+  it 'applies with no errors' do
+    apply_manifest(manifest, catch_failures: true)
+  end
+
+  [
+    'httpd',
+    'tomcat',
+  ].each do |service_name|
+    describe service(service_name) do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
+  end
+
+  describe port(80) do
+    it { is_expected.to be_listening }
+  end
+
+  describe port(443) do
+    it { is_expected.to be_listening }
+  end
+end
---
+++ spec/setup_acceptance_node.pp
@@ -0,0 +1,37 @@
+class { 'foreman::repo':
+  repo => 'nightly',
+}
+class { 'katello::repo':
+  repo_version => 'nightly',
+}
+class { 'candlepin::repo':
+  version => 'nightly',
+}
+
+user { 'vagrant':
+  ensure     => present,
+  managehome => true,
+}
+
+# Necessary for PostgreSQL EVR extension
+yumrepo { "pulpcore":
+  baseurl  => "http://yum.theforeman.org/pulpcore/3.14/el\$releasever/x86_64/",
+  descr    => "Pulpcore",
+  enabled  => true,
+  gpgcheck => true,
+  gpgkey   => "https://yum.theforeman.org/pulpcore/3.14/GPG-RPM-KEY-pulpcore",
+}
+
+if $facts['os']['release']['major'] == '8' {
+  package { 'glibc-langpack-en':
+    ensure => installed,
+  }
+
+  yumrepo { 'powertools':
+    enabled => true,
+  }
+} elsif $facts['os']['release']['major'] == '7' {
+  package { 'epel-release':
+    ensure => installed,
+  }
+}
