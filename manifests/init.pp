theforeman/puppet-katello_devel

commit aa27b00a649a47577768609f61e1c5226eb96f18

Drop foreman_certs class and let puppet-certs control
Results from this change https://github.com/theforeman/puppet-certs/pull/371

manifests/init.pp
@@ -154,6 +154,7 @@ class katello_devel (
   $fork_remote_name_real = pick_default($fork_remote_name, $github_username)
 
   $foreman_dir = "${deployment_dir}/foreman"
+  $foreman_cert_dir = "${deployment_dir}/foreman-certs"
 
   include certs
 
@@ -162,11 +163,30 @@ class katello_devel (
 
   include certs::pulp_client
 
+  file { $foreman_cert_dir:
+    ensure => directory,
+    owner  => $user,
+    group  => $group,
+    mode   => '0775',
+  }
+
+  class { 'certs::foreman':
+    client_cert => "${foreman_cert_dir}/client_cert.pem",
+    client_key  => "${foreman_cert_dir}/client_key.pem",
+    ssl_ca_cert => "${foreman_cert_dir}/proxy_ca.pem",
+    owner       => $user,
+    group       => $group,
+    before      => Class['katello_devel::config'],
+  }
+
+  $ssl_ca_file = "${foreman_cert_dir}/proxy_ca.pem"
+  $ssl_certificate = "${foreman_cert_dir}/client_cert.pem"
+  $ssl_priv_key = "${foreman_cert_dir}/client_key.pem"
+
   Class['certs'] ~>
   class { 'certs::apache': } ~>
   class { 'katello_devel::apache': } ~>
   class { 'katello_devel::install': } ~>
-  class { 'katello_devel::foreman_certs': } ~>
   class { 'katello_devel::config': } ~>
   class { 'katello_devel::database': }
 

manifests/foreman_certs.pp
@@ -1,45 +1,0 @@
-# @summary Class for setting up Foreman client certs
-# @api private
-class katello_devel::foreman_certs (
-  $hostname       = $certs::node_fqdn,
-  $generate       = $certs::generate,
-  $regenerate     = $certs::regenerate,
-  $deploy         = $certs::deploy,
-) {
-
-  $client_cert_name = "${hostname}-foreman-client"
-  $client_cert      = "${certs::pki_dir}/certs/${client_cert_name}.crt"
-  $client_key       = "${certs::pki_dir}/private/${client_cert_name}.key"
-  $ssl_ca_cert      = $certs::ca_cert
-
-  # cert for authentication of puppetmaster against foreman
-  cert { $client_cert_name:
-    hostname      => $hostname,
-    purpose       => client,
-    country       => $certs::country,
-    state         => $certs::state,
-    city          => $certs::city,
-    org           => 'FOREMAN',
-    org_unit      => 'PUPPET',
-    expiration    => $certs::expiration,
-    ca            => $certs::default_ca,
-    generate      => $generate,
-    regenerate    => $regenerate,
-    deploy        => $deploy,
-    password_file => $certs::ca_key_password_file,
-  }
-
-  Cert[$client_cert_name] ~>
-  pubkey { $client_cert:
-    key_pair => Cert[$client_cert_name],
-  } ~>
-  privkey { $client_key:
-    key_pair => Cert[$client_cert_name],
-  } ~>
-  file { $client_key:
-    group => $katello_devel::group,
-    owner => 'root',
-    mode  => '0440',
-  }
-
-}

spec/classes/katello_devel_spec.rb
@@ -29,6 +29,36 @@ describe 'katello_devel' do
         ]) }
 
         it do
+          verify_exact_contents(catalogue, '/home/vagrant/foreman/config/settings.yaml', [
+            ':unattended: true',
+            ':login: true',
+            ':require_ssl: false',
+            ':locations_enabled: true',
+            ':organizations_enabled: true',
+            ':oauth_active: true',
+            ':oauth_map_users: true',
+            ':oauth_consumer_key: OAUTH_KEY',
+            ':oauth_consumer_secret: OAUTH_SECRET',
+            ':ssl_ca_file: /home/vagrant/foreman-certs/proxy_ca.pem',
+            ':ssl_certificate: /home/vagrant/foreman-certs/client_cert.pem',
+            ':ssl_priv_key: /home/vagrant/foreman-certs/client_key.pem',
+            ':webpack_dev_server: true',
+            ':webpack_dev_server_https: true',
+            ':assets_debug: false',
+            ':loggers:',
+            '  :audit:',
+            '    :enabled: true',
+            '    :level: error',
+            '  :taxonomy:',
+            '    :enabled: true',
+            '    :level: error',
+            '  :dynflow:',
+            '    :enabled: true',
+            '    :level: info',
+          ])
+        end
+
+        it do
           verify_exact_contents(catalogue, '/home/vagrant/foreman/config/settings.plugins.d/katello.yaml', [
             ':katello:',
             '  :rest_client_timeout: 3600',
@@ -47,8 +77,8 @@ describe 'katello_devel' do
             '    :oauth_secret: OAUTH_SECRET',
             '    :ca_cert_file: /etc/pki/katello/certs/katello-default-ca.crt',
             '  :candlepin_events:',
-            '    :ssl_cert_file: /etc/pki/katello/certs/java-client.crt',
-            '    :ssl_key_file: /etc/pki/katello/private/java-client.key',
+            '    :ssl_cert_file: /home/vagrant/foreman-certs/client_cert.pem',
+            '    :ssl_key_file: /home/vagrant/foreman-certs/client_key.pem',
             '    :ssl_ca_file: /etc/pki/katello/certs/katello-default-ca.crt',
             '  :agent:',
             '    :broker_url: amqps://localhost:5671',

templates/settings.yaml.erb
@@ -14,9 +14,9 @@
 :oauth_consumer_secret: <%= scope['::katello_devel::oauth_secret'] %>
 
 # SSL settings
-:ssl_ca_file: <%= scope['::katello_devel::foreman_certs::ssl_ca_cert'] %>
-:ssl_certificate: <%= scope['::katello_devel::foreman_certs::client_cert'] %>
-:ssl_priv_key: <%= scope['::katello_devel::foreman_certs::client_key'] %>
+:ssl_ca_file: <%= scope['::katello_devel::ssl_ca_file'] %>
+:ssl_certificate: <%= scope['::katello_devel::ssl_certificate'] %>
+:ssl_priv_key: <%= scope['::katello_devel::ssl_priv_key'] %>
 
 # webpack dev server settings
 :webpack_dev_server: <%= scope['::katello_devel::webpack_dev_server'] %>

commit 6bf6aeddd5970b4a272f53e59e25b79a607ed5e4

Use variables for SSL paths
Previously this was duplicated. Now they are guaranteed to be in sync.

Fixes: aa27b00a649a47577768609f61e1c5226eb96f18

manifests/init.pp
@@ -161,6 +161,10 @@ class katello_devel (
   $candlepin_url = $katello::params::candlepin_url
   $candlepin_ca_cert = $certs::ca_cert
 
+  $ssl_ca_file = "${foreman_cert_dir}/proxy_ca.pem"
+  $ssl_certificate = "${foreman_cert_dir}/client_cert.pem"
+  $ssl_priv_key = "${foreman_cert_dir}/client_key.pem"
+
   include certs::pulp_client
 
   file { $foreman_cert_dir:
@@ -171,17 +175,13 @@ class katello_devel (
   }
 
   class { 'certs::foreman':
-    client_cert => "${foreman_cert_dir}/client_cert.pem",
-    client_key  => "${foreman_cert_dir}/client_key.pem",
-    ssl_ca_cert => "${foreman_cert_dir}/proxy_ca.pem",
+    client_cert => $ssl_certificate,
+    client_key  => $ssl_priv_key,
+    ssl_ca_cert => $ssl_ca_file,
     owner       => $user,
     group       => $group,
     before      => Class['katello_devel::config'],
   }
-
-  $ssl_ca_file = "${foreman_cert_dir}/proxy_ca.pem"
-  $ssl_certificate = "${foreman_cert_dir}/client_cert.pem"
-  $ssl_priv_key = "${foreman_cert_dir}/client_key.pem"
 
   Class['certs'] ~>
   class { 'certs::apache': } ~>