cc08208a87d3fd3d8fd6957cfc8f7cbad6b509ac
William Bradford Clark <wclark@redhat.com>
2024-06-05 15:16:28 -0400

    Use Pulpcore nightly in acceptance test

--- spec/setup_acceptance_node.pp
+++ spec/setup_acceptance_node.pp
@@ -15,11 +15,10 @@ user { 'vagrant':
 
 # Necessary for PostgreSQL EVR extension
 yumrepo { "pulpcore":
-  baseurl  => "http://yum.theforeman.org/pulpcore/3.14/el\$releasever/x86_64/",
+  baseurl  => "http://yum.theforeman.org/pulpcore/nightly/el\$releasever/x86_64/",
   descr    => "Pulpcore",
   enabled  => true,
-  gpgcheck => true,
-  gpgkey   => "https://yum.theforeman.org/pulpcore/3.14/GPG-RPM-KEY-pulpcore",
+  gpgcheck => false,
 }
 
 if $facts['os']['release']['major'] == '8' {
