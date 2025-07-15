ea0325373839f995130299a1f84f6847234a911e
Eric D. Helms <ericdhelms@gmail.com>
2025-07-14 20:52:02 -0400

    Drop EL7 and EL8 acceptance test setup

--- spec/setup_acceptance_node.pp
+++ spec/setup_acceptance_node.pp
@@ -12,25 +12,3 @@ user { 'vagrant':
   ensure     => present,
   managehome => true,
 }
-
-# Necessary for PostgreSQL EVR extension
-yumrepo { "pulpcore":
-  baseurl  => "http://yum.theforeman.org/pulpcore/nightly/el\$releasever/x86_64/",
-  descr    => "Pulpcore",
-  enabled  => true,
-  gpgcheck => false,
-}
-
-if $facts['os']['release']['major'] == '8' {
-  package { 'glibc-langpack-en':
-    ensure => installed,
-  }
-
-  yumrepo { 'powertools':
-    enabled => true,
-  }
-} elsif $facts['os']['release']['major'] == '7' {
-  package { 'epel-release':
-    ensure => installed,
-  }
-}
